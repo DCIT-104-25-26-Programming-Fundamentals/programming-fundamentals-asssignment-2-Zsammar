de69cea4dbf5336ad057de1bdb56680c89751669
Zsammar <saahmed006@st.ug.edu.gh>
2026-08-02 11:32:06 +0000

    assignment 3 done.

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,70 @@
 #include <iostream>
 using namespace std;
 
+// finding sum
+int findSum(int arr[], int n) {
+  int sum = 0;
+  for (int i = 0; i < n; i++){
+    sum += arr[i];
+  }
+return sum;
+}
+
+//finding maximum value
+int findMax(int arr[], int n) {
+  int maxNum = arr[0];
+  for (int i = 1; i < n; i++){
+    if(arr[i] > maxNum) {
+      maxNum = arr[i];
+    }
+  }
+  return maxNum;
+}
+
+
+//finding minimum value
+int findMin(int arr[], int n) {
+  int minNum = arr[0];
+  for (int i = 1; i < n; i++) {
+    if (arr[i] < minNum) {
+      minNum = arr[i];
+    }
+  }
+  return minNum;
+
+}
+
+
+//finding average
+double findAvg(int arr[], int n) {
+  return static_cast<double>(findSum(arr, n)) /n;
+}
+
+
+
+int main() {
+  int n;
+  int i;
+  cout << "How many numbers? ";
+  cin >> n;
+
+  if (n <= 0 ) {
+    cout << "Error: N must be a positive integer." << endl;
+    return 0;
+  }
+
+int arr[n];
+for (i = 0; i < n; i++) {
+  cout << "Enter number " << (i + 1) << ": ";
+  cin >> arr[i];
+}
+
+cout << endl;
+cout << "Results: " << endl;
+cout << "Sum: " << findSum(arr, n) << endl;
+cout << "Average: " << findAvg(arr, n) << endl;
+cout << "Maximum: " << findMax(arr, n) << endl;
+cout << "Minimum: " << findMin(arr, n) << endl;
+
+return 0;
+}
