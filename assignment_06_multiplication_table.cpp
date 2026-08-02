f6cc5a8b6947348336c32e11e544ba38c3573e5d
Zsammar <saahmed006@st.ug.edu.gh>
2026-08-02 17:33:42 +0000

    assignment 6 done.

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -55,5 +55,54 @@
 // =============================================================================
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+//multiplication table from 1 - 12 for a single number
+void printSingleTable(int number) {
+     cout << "Multiplication Table for " << number << ": " <<endl;
+     for (int i = 1; i <= 12; i++) {
+      cout << number << " x " << setw(2) << i << " = " << (number * i) << endl;
+     }
+}
+
+
+// multiplication table from 1 - n
+void printTablesToN(int n) {
+  if (n <= 0) {
+    cout << "Error: N must be a positive integer." << endl;
+    return;
+  }
+
+  for (int num = 1; num <= n; num++) {
+    printSingleTable(num);
+
+    if (num != n) {
+      cout << "---------------------------" << endl;
+
+    }
+  }
+}
+
+
+
+int main() {
+  // --------------PART A--------------------
+  int number;
+  cout << "Enter a number: ";
+  cin >> number;
+  cout << endl;
+
+  printSingleTable(number);
+  cout << endl;
+
+
+  // --------------PART B--------------------
+  int n;
+  cout << "Enter a number N: ";
+  cin >> n;
+  cout << endl;
+
+  printTablesToN(n);
+  return 0;
+}
